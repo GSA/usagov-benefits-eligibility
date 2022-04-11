0767dcd6f692b875cb7bc9617c6bd801178a7576
Luke Garceau <luke.garceau@gsa.gov>
2022-04-11 10:48:53 -0400

    Revert "tMerge branch 'luke_location_input' of https://github.com/GSA/usagov-benefits-eligibility into luke_location_input"
    This reverts commit b8a802f6f4d88a6058728e0c02b0a79aec4c3745, reversing
    changes made to 3a2f094dabddc59b416581dca2fbac95e08600be.

--- services/locationHelper.js
+++ services/locationHelper.js
@@ -2,16 +2,20 @@ import zipcodes from "zipcodes"
 import zipnrviens from "zipcodes-nrviens"
 import axios from "axios"
 
-function validateLocation({ values, response, callback }) {
+function validateLocation({ values, response, callback }) {  
   if (values) {
-    if (!values.some((value) => ["ih", "ia", "pa", "hm"].indexOf(value))) {
+    if(!values.some(value => ['ih', 'ia', 'pa', 'hm'].indexOf(value))) {
       switch (values.length) {
         case 1:
           return values[0] === response
         case 2: {
           // need to pull zip code and radius
-          let radius = values[0].includes("mi") ? values[0] : values[1]
-          const startingZip = values[0].includes("mi") ? values[1] : values[0]
+          let radius = values[0].includes("mi")
+            ? values[0]
+            : values[1]
+          const startingZip = values[0].includes("mi")
+            ? values[1]
+            : values[0]
           radius = radius.split("mi")[0]
           return zipcodes.distance(parseInt(response), parseInt(startingZip)) < radius
         }
@@ -21,19 +25,19 @@ function validateLocation({ values, response, callback }) {
     } else {
       checkZip(response, callback, values[0])
     }
-  }
+  } 
   checkZip(response, callback)
   return null
 }
 
-function checkZip(userResponse, callback, program = null) {
+function checkZip(userResponse, callback, program=null) {
   const zipCodeInfo = zipnrviens.lookup(parseInt(userResponse))
   const { state, county } = zipCodeInfo
   let requestUrl = `https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries?$filter=(state eq '${state}' and declaredCountyArea eq '${county} (County)' and incidentEndDate eq ''`
-  if (program !== null) {
-    requestUrl += " and " + program + "ProgramDeclared eq true)"
+  if(program !== null){
+    requestUrl += ' and ' + program + "ProgramDeclared eq true)"
   } else {
-    requestUrl += ")"
+    requestUrl += ')'
   }
   axios
     .get(requestUrl)
